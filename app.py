7717a24716698e0540ff80030448f93ca3747dff
nfinkle <nfinkle@princeton.edu>
2023-04-24 17:38:22 -0400

    new app script format route

--- app.py
+++ app.py
@@ -165,6 +165,11 @@ def autocorrected_words():
 
     return render_template('autocorrected_words.html', corrected=sortDict(alt_spellings),  italicized=italicized_words, num_words=len(alt_spellings.keys()), num_suggestions=suggestions)
 
+@app.route('/_app_script_format')
+def app_script_format():
+    alt_spellings, italicized_words, suggestions = get_all_words()
+    return render_template('app_script_format.html', corrected=sortDict(alt_spellings),  italicized=italicized_words, num_words=len(alt_spellings.keys()), num_suggestions=suggestions)
+
 @app.route('/_word_dump')
 def word_dump():
     alt_spellings, italicized_words, suggestions = get_all_words()
